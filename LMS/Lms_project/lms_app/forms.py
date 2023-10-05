d2e8fa5150793ebc3dd7b42b58c1d8729e54ade5
MidhunBabu01 <midhunkb57@gmail.com>
2023-10-05 18:10:26 +0530

    updates

--- LMS/Lms_project/lms_app/forms.py
+++ LMS/Lms_project/lms_app/forms.py
@@ -7,10 +7,10 @@ class AddCourse(forms.ModelForm):
         model = Coursess
         exclude = ['added_by']
         widgets = {
-            'title' : forms.TextInput(attrs={'class':'form-control'}),
-            'price' : forms.NumberInput(attrs={'class':'form-control'}),
-            'img' : forms.ClearableFileInput(attrs={'class':'form-control'}),
-            'discount_price' : forms.NumberInput(attrs={'class':'form-control'}),
-            'course_categories' : forms.Select(attrs={'class':'form-control'}),
+            'title' : forms.TextInput(attrs={'class':'form-control','required':'required'}),
+            'price' : forms.NumberInput(attrs={'class':'form-control','required':'required'}),
+            'img' : forms.ClearableFileInput(attrs={'class':'form-control','required':'required'}),
+            'discount_price' : forms.NumberInput(attrs={'class':'form-control','required':'required'}),
+            'course_categories' : forms.Select(attrs={'class':'form-control','required':'required'}),
             'about_course': forms.Textarea(attrs={'class':'form-control'}),
         }
